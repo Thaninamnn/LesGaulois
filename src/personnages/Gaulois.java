c09fd4c54bd7a5c3c69fc355534c91faf8dbf80e
Thanina Menoun <mnt4610a@ensfsi.onmicrosoft.com>
2024-09-19 15:03:42 +0200

    class gaulois

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,46 @@
+package personnages;
+
+public class Gaulois {
+	private String nom;
+	private int force;
+	private int effetpotion = 1;
+
+	public Gaulois(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+	
+	
+	public void parler(String texte) {
+		System.out.println(prendreparole()+"« " + texte + "»");
+	}
+	
+	private String prendreparole() {
+		return "Le gaulois " + nom + " : ";
+	
+	}
+	 public void frapper(Romain romain) {
+		System.out.println(nom + " envoie un grand coup dans la mâchoire de "
+				+ romain.getNom());
+	}
+	
+	
+	
+	
+	
+
+
+
+
+	@Override
+	public String toString() {
+		return "Gaulois [nom=" + nom + ", force=" + force + ", effetpotion=" + effetpotion + "]";
+	}
+
+	public static void main(String[] args) {
+
+	}}
